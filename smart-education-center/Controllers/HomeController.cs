634b6d3e7658ba1fa448ac9890376022d7d593a4
Abdullah Mansoor <mansoorabdullah001@gmail.com>
2019-02-25 21:38:30 +0530

    Initial Commit

---
+++ smart-education-center/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace smart_education_center.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ smart-education-center/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="smart_education_center.MvcApplication" Language="C#" %>
