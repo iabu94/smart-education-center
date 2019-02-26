b341f706adccabcb2960a35bbb4f6e4db916e81e
Thilina Basnayaka <imtmb323@gmail.com>
2019-02-26 12:38:39 +0530

    examcontroller, index page, view subject page added

---
+++ smart-education-center/Controllers/ExamsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace smart_education_center.Controllers
+{
+    public class ExamsController : Controller
+    {
+        // GET: Exams
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SubjectView(int gradeID)
+        {
+            TempData["gradeID"] = gradeID;
+            return View();
+        }
+    }
+}
