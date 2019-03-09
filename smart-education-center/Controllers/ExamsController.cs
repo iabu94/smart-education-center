10d1cd11127f548f5d01deb5e9dac07fb92aa0b4
Thilina Basnayaka <imtmb323@gmail.com>
2019-03-09 13:14:16 +0530

    Added paperview

--- smart-education-center/Controllers/ExamsController.cs
+++ smart-education-center/Controllers/ExamsController.cs
@@ -11,12 +11,40 @@ namespace smart_education_center.Controllers
         // GET: Exams
         public ActionResult Index()
         {
+            List<string> list = new List<string>();
+            list.Add("Grade 6");
+            list.Add("Grade 7");
+            list.Add("Grade 8");
+            list.Add("Grade 9");
+            list.Add("Grade 10");
+            list.Add("Grade 11");
+            ViewBag.gradeList = list;
             return View();
         }
 
         public ActionResult SubjectView(int gradeID)
         {
-            TempData["gradeID"] = gradeID;
+            Session["GradeID"] = gradeID;
+            return View();
+        }
+
+        public ActionResult PaperView(int subjectID)
+        {
+            if (Session["GradeID"] == null)
+            {
+                return RedirectToAction("Index", "Exams");
+            }
+            Session["SubjectID"] = subjectID;
+            return View();
+        }
+
+        public ActionResult QuestionView(int paperID)
+        {
+            if (Session["GradeID"] == null || Session["SubjectID"] ==null)
+            {
+                return RedirectToAction("Index", "Exams");
+            }
+            Session["PaperID"] = paperID;
             return View();
         }
     }
